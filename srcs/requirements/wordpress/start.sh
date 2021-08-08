7b0c653a6d92ba4b4870433a0c6138e626a09727
Ruslan Osmaev <osmaev.ruslan.bmstu@gmail.com>
2021-08-08 07:18:45 +0300

    update

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -1,5 +1,8 @@
-cp -r /wordpress/* /var/lib/wordpress/
-rm -rf wordpress/
+rm -rf /var/lib/wordpress/*
+wget https://ru.wordpress.org/latest-ru_RU.tar.gz
+tar -xzf /latest-ru_RU.tar.gz -C /var/lib/ ; rm -rf /latest-ru_RU.tar.gz
+cp /wp-config.php /var/lib/wordpress/
+rm -rf /wp-config.php
 chown -R www-data:www-data /var/lib/*
 chmod -R 755 /var/lib/*
 /usr/sbin/php-fpm7.3 -F
